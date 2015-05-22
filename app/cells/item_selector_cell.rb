c021b283ed92115c3c64f74aa05c159e6637a048
Marnen Laibow-Koser <marnen@marnen.org>
2015-05-22 00:16:47 -0400

    Translate item names. [#94638328]

--- app/cells/item_selector_cell.rb
+++ app/cells/item_selector_cell.rb
@@ -1,7 +1,7 @@
 class ItemSelectorCell < Cell::Rails
   def show(args)
     @form = args[:form]
-    @items = Item.order :name
+    @items = Item.order(:name).collect {|item| [_(item.name), item.id] }
     render
   end
 end
