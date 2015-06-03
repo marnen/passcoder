07adbc856d4a72e075ac27ff7187d1e37ca7a8db
Marnen Laibow-Koser <marnen@marnen.org>
2015-06-03 17:27:41 -0400

    We only need to permit and require once. [#94638328]

--- app/controllers/passcodes_controller.rb
+++ app/controllers/passcodes_controller.rb
@@ -11,10 +11,11 @@ class PasscodesController < ApplicationController
   end
 
   def create
-    @passcode = Passcode.new params.require(:passcode).permit *Passcode.permitted_params
+    passcode_params = params.require(:passcode).permit *Passcode.permitted_params
+    @passcode = Passcode.new passcode_params
     unless @passcode.save
-      params.require(:passcode).require(:line_items_attributes).each do |line_item|
-        @passcode.line_items.build line_item.permit *LineItem.permitted_params
+      passcode_params[:line_items_attributes].each do |line_item|
+        @passcode.line_items.build line_item
       end
     end
     respond_with @passcode
